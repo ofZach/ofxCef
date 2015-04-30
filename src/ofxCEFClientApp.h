e2e173851300d29257ad60dff539af38d9e51599
Hugues Bruyere <hbruyere@gmail.com>
2015-04-30 10:56:31 -0400

    Added some command line flags. The following one '--disable-d3d11` changed the performance quite a lot on Windows. (Still need to see if this last flag need to be omitted in ofxCEF when on OS X or if we let CEF deal with this).

--- src/ofxCEFClientApp.h
+++ src/ofxCEFClientApp.h
@@ -17,6 +17,40 @@ public:
 
     void OnWebKitInitialized() OVERRIDE;
 
+	virtual void OnBeforeCommandLineProcessing( const CefString& process_type, CefRefPtr<CefCommandLine> command_line) {
+
+		CefString osr(L"-off-screen-rendering-enabled");
+		command_line->AppendSwitch(osr);
+
+		CefString d3d11(L"-disable-d3d11");
+		command_line->AppendSwitch(d3d11);
+
+		CefString frameScheduling(L"-enable-begin-frame-scheduling");
+		command_line->AppendSwitch(frameScheduling);
+
+		//CefString singleProcess(L"-single-process");
+		//command_line->AppendSwitch(singleProcess);
+
+		//CefString igProxy(L"-no-proxy-server");
+		//command_line->AppendSwitch(igProxy);
+
+		//CefString addFile(L"-allow-file-access-from-files");
+		//command_line->AppendSwitch(addFile);
+
+		//CefString touchEventsDisabled(L"-touch-events=disabled");
+		//command_line->AppendSwitch(touchEventsDisabled);
+
+		//CefString screenCap(L"-allow-http-screen-capture");
+		//command_line->AppendSwitch(screenCap);
+
+		//CefString optimizedUI(L"-touch-optimized-ui=disabled");
+		//command_line->AppendSwitch(optimizedUI);
+
+		// CefString touchSimulated(L"-simulate-touch-screen-with-mouse");
+		// command_line->AppendSwitch(touchSimulated);
+
+	}
+
     IMPLEMENT_REFCOUNTING(ofxCEFClientApp);
 };
 
